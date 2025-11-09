377236b7e94a55ed82ab57d2bb375ee95d130e90
Lisa <cse_1912020239@lus.ac.bd>
2025-11-09 10:02:40 +0600

    Text, Image

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -42,6 +43,26 @@ class HomePage extends StatelessWidget {
             title: Text("Profile"),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              "Hello",
+              style: GoogleFonts.lobster(
+                textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Image.asset(
+              'assets/images/images.jpeg',
+              height: 400,
+              fit: BoxFit.fill,
+            ),
+            Image.network(
+              "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
+            ),
+          ],
+        ),
       ),
     );
   }
